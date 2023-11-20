b6f36eeaffedb125f074c726ad5888b56cb19a87
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-11-20 19:45:43 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
-        print("{} {} = {}".format(a, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
     elif op == "/":

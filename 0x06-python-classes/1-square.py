14fca0504e7146be1e83e8b389cc20983f376ed6
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-11-21 22:32:04 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
-#!/usr/bin.python3
+#!/usr/bin/python3
 
 class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size

9d415d0ce7e66e2471756226890dbc83765f57a3
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-11-21 23:33:18 +0200

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -41,3 +41,10 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+    def area(self):
+        """Area of.
+
+        Returns:
+            The size.
+        """
+        return self.__size ** 2
